84dcb32be07348804e3071fc3cdc766d361af6e8
EunYeol <163628847+giteunyeol@users.noreply.github.com>
2025-01-13 15:10:45 +0900

    Add files via upload

---
+++ dataformat.cs
@@ -0,0 +1,21 @@
+namespace Csharp
+{
+    class Program
+    {
+        // 주석 : // 
+        static void Main(string[] args)
+        {
+            //int
+            //float 
+            //string
+            //bool
+
+            // [데이터 타입][이름];
+            int hp = 100;
+            int maxHp;
+            
+
+            Console.WriteLine(hp);
+        }
+    }
+}
