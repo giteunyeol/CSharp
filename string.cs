97837b13a33464b14c33fc702c7e76a4ccb56182
EunYeol <163628847+giteunyeol@users.noreply.github.com>
2025-01-13 15:14:10 +0900

    Add files via upload

---
+++ string.cs
@@ -0,0 +1,19 @@
+namespace string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string
+            string name;
+            name = "EunYeol";
+
+            char ch;
+            ch = 'E';
+
+            Console.WriteLine(name);
+            
+        }
+
+    }
+}
