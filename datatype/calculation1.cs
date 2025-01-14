305c6f134092a59a1d6a29a67c91872ffb4765ba
EunYeol <163628847+giteunyeol@users.noreply.github.com>
2025-01-14 16:56:20 +0900

    Add files via upload

---
+++ datatype/calculation1.cs
@@ -0,0 +1,18 @@
+//산술 연산 
+// + - * / %
+
+// [ 100 ]
+int hp = 100;
+// 1) 할당     int hp
+// 2) write   hp = 100;
+// 3) read    hp 
+int value = 5;
+hp = hp + value;
+
+Console.WriteLine(hp);
+
+// 몫, 나머지
+int a = 10 / 3; //3
+int b = 10 % 3; //1
+Console.WriteLine(a);
+Console.WriteLine(b);
