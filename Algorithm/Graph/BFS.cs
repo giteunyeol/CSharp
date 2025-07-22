8be2396065b1295aa1362163ae97d0b0004539a2
EunYeol <163628847+giteunyeol@users.noreply.github.com>
2025-07-22 15:10:58 +0900

    Add files via upload

---
+++ Algorithm/Graph/BFS.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise
+{
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            {0, 1, 0, 1, 0, 0},
+            {1, 0, 1, 1, 0, 0},
+            {0, 1, 0, 0, 0, 0},
+            {1, 1, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0},
+        };
+
+        List<int>[] adj2 = new List<int>[] //c++ vector
+        {
+            new List<int>() { 1, 3 },
+            new List<int>() { 0, 2, 3 },
+            new List<int>() { 1 },
+            new List<int>() { 0, 1, 4},
+            new List<int>() { 3, 5 },
+            new List<int>() { 4 },
+        };
+
+        public void BFS(int start) // 예약시스템 이용, 큐, 배열
+        {
+            bool[] found = new bool[6];
+            int[] parent =  new int[6];
+            int [] distance = new int[6];
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+            parent[start] = start;
+            distance[start] = 0;;
+            while (q.Count > 0)
+            {
+                int now = q.Dequeue();
+                System.Console.WriteLine(now);
+                
+                for(int next = 0; next < 6; next++)
+                {
+                    if(adj[now, next] == 0) //인접하지 않았으면스킵
+                    {
+                        continue;
+                    }
+                    if(found[next])
+                    {
+                        continue;
+                    }
+                    q.Enqueue(next);
+                    found[next] = true;
+                    parent[next] = now;
+                    distance[next] = distance[now] + 1;
+                }
+
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DFS (Depth First Search 깊이 우선 탐색)
+            // BFS (Breadth First Search 너비 우선 탐색) 최단거리(길찾기때 사용)
+            Graph graph = new Graph();
+            graph.BFS(0);
+
+        }
+    }
+}
---
+++ Algorithm/Graph/BFSFind.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Pos
+    {
+        public Pos(int y, int x) { Y = y; X = x;}
+        public int Y;
+        public int X;
+    }
+
+    class Player
+    {
+        public int PosX { get; private set; }
+        public int PosY { get; private set; }
+        Random _random = new Random();
+        Board _board;
+
+        enum Dir
+        {
+            Up = 0, // 3
+            Left = 1, // 0
+            Down = 2, // 1
+            Right = 3 // 2
+        }
+
+        int _dir = (int)Dir.Up;
+        List<Pos> _points = new List<Pos>();
+
+        public void Initialize(int posY, int posX, Board board)
+        {
+            PosX = posX;
+            PosY = posY;
+            _board = board;
+
+            BFS();
+        }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] {-1, 0, 1, 0};
+            int[] deltaX = new int[] {0, -1, 0, 1};
+            bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> q = new Queue<Pos>();
+            q.Enqueue(new Pos(PosY, PosX));
+            found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (q.Count> 0)
+            {
+                Pos pos = q.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for(int i = 0; i < 4; i++)
+                {
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                    {
+                        continue;
+                    }
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                    {
+                        continue;
+                    }
+                    if (found[nextY, nextX])
+                    {
+                        continue;
+                    }
+                    
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+                }
+            }
+
+            int y = _board.DestY;
+            int x = _board.DestX;
+            while (parent[y, x].Y != y || parent[y,x].X != x)
+            {
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y,x];
+                y = pos.Y;
+                x = pos.X;
+            }
+            _points.Add(new Pos(y, x));
+            _points.Reverse();
+        }
+
+        void RightHand()
+        {
+            //현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다.
+            int[] frontY = new int[] { -1, 0, 1, 0 };
+            int[] frontX = new int[] { 0, -1, 0, 1 };
+            int[] rightY = new int[] { 0, -1, 0, 1 };
+            int[] rightX = new int[] { 1, 0, -1, 0 };
+
+            _points.Add(new Pos(PosY, PosX));
+            //목적지 도착하기 전에는 계속 실행
+            while (PosY != _board.DestY || PosX != _board.DestX)
+            {
+                // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
+                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
+                {
+                    // 오른쪽 방향으로 90도 회전
+                    _dir = (_dir - 1 + 4) % 4;
+                    // 앞으로 한 보 전진
+                    PosX = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                // 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+                {
+                    // 앞으로 한 보 전진.
+                    PosX = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                else
+                {
+                    // 왼쪽 방향으로 90도 회전
+                    _dir = (_dir + 1 + 4) % 4;
+                }
+            }
+        }
+
+        const int MOVE_TICK = 10; //0.1초
+        int _sumTick = 0;
+        int _lastIndex = 0;
+        public void Update(int deltaTick)
+        {
+            if (_lastIndex >= _points.Count)
+            return;
+
+            _sumTick += deltaTick;
+            if (_sumTick >= MOVE_TICK)
+            {
+                _sumTick = 0;
+
+                PosY = _points[_lastIndex].Y;
+                PosX = _points[_lastIndex].X;
+                _lastIndex++;
+            }
+        }
+
+    }
+}
